eb107f1143e8bdbf497a4f0650fa0e3ab6c9825f
HotBit <treibrazi.imobiliare@gmail.com>
2026-05-05 23:21:46 +0300

    Remove ProtectedLayout from user profile layout

--- app/profile/[userId]/layout.jsx
+++ app/profile/[userId]/layout.jsx
@@ -1,3 +1,1 @@
-'use client'
-import ProtectedLayout from '../../../components/ProtectedLayout'
-export default function Layout({ children }) { return <ProtectedLayout>{children}</ProtectedLayout> }
+export default function Layout({ children }) { return children }
