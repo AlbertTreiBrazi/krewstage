b35a48d82f0958180a265f57e67d81e414249d54
HotBit <treibrazi.imobiliare@gmail.com>
2026-05-06 20:27:08 +0300

    Wrap children in ProtectedLayout component

--- app/projects/new/layout.jsx
+++ app/projects/new/layout.jsx
@@ -1,1 +1,3 @@
-export default function Layout({ children }) { return children }
+'use client'
+import ProtectedLayout from '../../../components/ProtectedLayout'
+export default function Layout({ children }) { return <ProtectedLayout>{children}</ProtectedLayout> }
